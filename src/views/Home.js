5c7bf0f3cfeb7587cf3b21003e6163ef9847dd9c
shtaimoortariq <taimoor_t@yahoo.com>
2020-06-19 13:44:19 +0500

    todos only shows when login

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
+import { useAuth0 } from "../react-auth0-spa";
 
-// import Hero from "../components/Hero";
-// import Content from "../components/Content";
+import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 const GET_MY_TODOS = gql`
   query getMyTodos {
@@ -15,6 +16,7 @@ const GET_MY_TODOS = gql`
 `;
 
 const Home = () => {
+  const { isAuthenticated } = useAuth0();
   const { loading, error, data } = useQuery(GET_MY_TODOS);
   console.log(data, "data");
   if (loading) {
@@ -26,9 +28,14 @@ const Home = () => {
   }
   return (
     <Fragment>
-      {data.todos.map((todo, index) => (
-        <div key={index}>{todo.todo}</div>
-      ))}
+      {isAuthenticated ? (
+        data.todos.map((todo, index) => <div key={index}>{todo.todo}</div>)
+      ) : (
+        <div>
+          <Hero />
+          <Content />
+        </div>
+      )}
     </Fragment>
   );
 };
